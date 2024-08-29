5aba53caea0b50b3b357a6f5a693dcea45d43489
acatejr <acatejr@gmail.com>
2024-08-29 15:08:03 -0700

    Fixed config import

--- dadaforest/urls.py
+++ dadaforest/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic.base import RedirectView
+import config
 
 urlpatterns = [
     path("", RedirectView.as_view(url="/admin")),
